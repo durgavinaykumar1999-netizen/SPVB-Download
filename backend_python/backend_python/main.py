f45458723ef99cf9022f1aaea3db0d784b1b3358
durgavinaykumar1999 <vinay@aiplex.in>
2026-08-07 19:13:43 +0530

    Fix CORS error - set allow_credentials to False for wildcard origins

--- backend_python/backend_python/main.py
+++ backend_python/backend_python/main.py
@@ -59,7 +59,7 @@ async def shutdown_event():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
